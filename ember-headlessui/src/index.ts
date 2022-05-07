1db97b22523b9846aa4053d099ce663a4a00eb43
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-05-07 13:05:53 -0400

    Resolves: #94, re-export public API components from index

---
+++ ember-headlessui/src/index.ts
@@ -0,0 +1,5 @@
+export { default as Dialog } from './components/dialog';
+export { default as Listbox } from './components/listbox';
+export { default as Menu } from './components/menu';
+export { default as Switch } from './components/switch';
+export { default as Transition } from './components/transition';
